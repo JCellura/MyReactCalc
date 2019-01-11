a90cecf87c3049202ff7bf78185dface27f34661
Joe Cellura <joecellura@Joes-MacBook-Air.local>
2019-01-11 00:40:03 -0500

    Updates

---
+++ precache-manifest.045145962addd95a52887b4c086e4453.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ef177444804fd7ff695",
+    "url": "/MyReactCalc/static/js/runtime~main.3ef17744.js"
+  },
+  {
+    "revision": "e03e33980d36ad63873f",
+    "url": "/MyReactCalc/static/js/main.e03e3398.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/MyReactCalc/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "e03e33980d36ad63873f",
+    "url": "/MyReactCalc/static/css/main.1c8496b5.chunk.css"
+  },
+  {
+    "revision": "6aa060f9c85f5ab4425ec163e1288d25",
+    "url": "/MyReactCalc/index.html"
+  }
+];
